139e12ae9b155adbd867319b11c7876a3fabb324
Alan Hamlett <alan.hamlett@gmail.com>
2020-10-15 18:01:52 -0700

    fix bugs

--- scrape.ts
+++ scrape.ts
@@ -1,21 +1,25 @@
 import axios from 'axios';
 import * as core from '@actions/core';
 
+type AQIRange = {
+  cutoff: number;
+  label: string;
+};
+
 function getAqiLabel(pm25: number): string {
-  const aqiTable = [
-    [12, 'good'], // AQI 0-50
-    [35.4, 'moderate'], // AQI 51-100
-    [55.4, 'unhealthy for sensitive groups'], // AQI 101-150
-    [150.4, 'unhealthy'], // AQI 151-200
-    [250.4, 'very unhealthy'], // AQI 201-300
-    [350.4, 'hazardous'], // AQI 301+
+  const aqiTable: AQIRange[] = [
+    { cutoff: 12, label: 'good' }, // AQI 0-50
+    { cutoff: 35.4, label: 'moderate' }, // AQI 51-100
+    { cutoff: 55.4, label: 'unhealthy for sensitive groups' }, // AQI 101-150
+    { cutoff: 150.4, label: 'unhealthy' }, // AQI 151-200
+    { cutoff: 250.4, label: 'very unhealthy' }, // AQI 201-300
+    { cutoff: 350.4, label: 'hazardous' }, // AQI 301+
   ];
 
   for (var row of aqiTable) {
-    const cutoff = row[0];
-    if (pm25 <= cutoff) return row[1];
+    if (pm25 <= row.cutoff) return row.label;
   }
-  return aqiTable[-1][1];
+  return aqiTable[-1].label;
 }
 
 export const scrape = async () => {
@@ -23,19 +27,16 @@ export const scrape = async () => {
 
   const url = 'https://www.purpleair.com/json?show=' + sensor_id;
   const response = await axios.get(url);
-  const pm25: number = parseFloat(response.data.results[0].pm2_5_cf_1);
-  console.log('PM2.5: ' + pm25.toString());
-  console.log(`::set-output name=pm25::${pm25}`);
+  const pm25 = parseFloat(response.data.results[0].pm2_5_cf_1);
   const aqi = getAqiLabel(pm25);
-  console.log('AQI: ' + aqi);
 
   const PM25_THRESHOLD = 100;
   if (pm25 >= PM25_THRESHOLD) {
-    const msg = `Air quality ${label}. PM2.5 over ${PM25_THRESHOLD} threshold!`;
+    const msg = `Air quality ${aqi}. PM2.5 ${pm25} over ${PM25_THRESHOLD} threshold!`;
     console.log(msg);
     core.setFailed(msg);
   } else {
-    console.log(`Air quality ${label}. PM2.5 under threshold ${PM25_THRESHOLD}.`);
+    console.log(`Air quality ${aqi}. PM2.5 ${pm25} under threshold ${PM25_THRESHOLD}.`);
   }
 };
 
